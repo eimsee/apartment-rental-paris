4c9f0fbd340bcd14989d83b74608aebbf713d507
marie christine <eimsee_eesmie@live.fr>
2022-12-12 05:35:37 -0400

    ok

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,14 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
   def home
+    @offers = Offer.all
+    @offer = Offer.where(id: params[:id])
+    @markers = @offers.geocoded.map do |offer|
+      {
+        lat: offer.latitude,
+        lng: offer.longitude,
+        info_window: render_to_string(partial: "info_window", locals: { offer: offer})
+      }
+    end
   end
 end
